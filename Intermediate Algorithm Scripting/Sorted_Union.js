8b61873ddf5e827ccdcfd5462d405a84313ca703
Yue L. Chen <12385495+yuelchen@users.noreply.github.com>
2018-05-12 23:37:45 -0400

    Create Sorted_Union.js
    Sorted Union Challenge Link:
    https://www.freecodecamp.org/challenges/sorted-union

---
+++ Intermediate Algorithm Scripting/Sorted_Union.js
@@ -0,0 +1,28 @@
+/* Mission:
+ * Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+ * The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+ *
+ * Resources:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+ */
+
+function uniteUnique(){
+  var unique = [];
+  for(i=0;i<arguments.length;i++){
+    var array = arguments[i];
+    for(j=0;j<array.length;j++)
+      if(unique.indexOf(array[j]) == -1)
+        unique.push(array[j]);
+  }
+  return unique;
+}
+
+/* =============================================================================
+ * Example Test Cases
+ * =============================================================================
+ */
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);             //should return [1, 3, 2, 5, 4]
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]);               //should return [1, 3, 2, [5], [4]]
+uniteUnique([1, 2, 3], [5, 2, 1]);                        //should return [1, 2, 3, 5]
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);  //should return [1, 2, 3, 5, 4, 6, 7, 8]
