31c0f8e35cea9b24695367107fa54e2e771f4fdf
Yue L. Chen <12385495+yuelchen@users.noreply.github.com>
2018-05-16 00:56:05 -0400

    Create Repeat_A_String_Repeat_A_String.js
    Repeat A String Repeat A String Challenge Link:
    https://www.freecodecamp.org/challenges/repeat-a-string-repeat-a-string

---
+++ Basic Algorithm Scripting/Repeat_A_String_Repeat_A_String.js
@@ -0,0 +1,22 @@
+/* Mission:
+ * Repeat a given string (first argument) num times (second argument). 
+ * Return an empty string if num is not a positive number.
+ *
+ * Resources:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+ */
+
+function repeatStringNumTimes(str, num) {
+  return num >= 0 ? str.repeat(num) : "";
+}
+
+/* =============================================================================
+ * Example Test Cases
+ * =============================================================================
+ */
+repeatStringNumTimes("*", 3);     //should return "***"
+repeatStringNumTimes("abc", 3);   //should return "abcabcabc"
+repeatStringNumTimes("abc", 4);   //should return "abcabcabcabc"
+repeatStringNumTimes("abc", 1);   //should return "abc"
+repeatStringNumTimes("*", 8);     //should return "********"
+repeatStringNumTimes("abc", -2);  //should return ""
