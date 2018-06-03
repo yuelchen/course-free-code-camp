706357208adf4a018d323cb78a09068a22ff627d
Yue L. Chen <12385495+yuelchen@users.noreply.github.com>
2018-06-03 00:12:13 -0400

    Create index.js

---
+++ Front End Projects/Basic/Daily Mood Survey/index.js
@@ -0,0 +1,7 @@
+function submitted(){
+  var name = document.getElementById("name").value;
+  if(name != ""){
+    alert("Oh happy day, thank you for your contribution " + name +".\nHope to see you soon again!");
+    window.location.href = "https://codepen.io/yuelchen/full/PaPgxX/";
+  }
+}
