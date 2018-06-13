95499f7ce4d04d7e4b8fc2a5a3b46187e8fdd699
yuelchen <12385495+yuelchen@users.noreply.github.com>
2018-06-13 00:35:47 -0400

    Removing projects due to duplicate solutions

--- Front End Projects/Basic/A Tribute Page/index.js
+++
@@ -1,9 +0,0 @@
-function show(x){
-  var id = x + "-details";
-  var details = "#" + x + "-details";
-  var display = document.getElementById(id).style.display;
-  if(display == "block")
-    $( details ).slideUp();
-  else
-    $( details ).slideDown();
-}
--- Front End Projects/Basic/Daily Mood Survey/index.js
+++
@@ -1,7 +0,0 @@
-function submitted(){
-  var name = document.getElementById("name").value;
-  if(name != ""){
-    alert("Oh happy day, thank you for your contribution " + name +".\nHope to see you soon again!");
-    window.location.href = "https://codepen.io/yuelchen/full/PaPgxX/";
-  }
-}
