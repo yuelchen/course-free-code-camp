c38f476a4956547953f8f042ad1564e2c5b20c22
yuelchen <12385495+yuelchen@users.noreply.github.com>
2018-06-13 00:48:52 -0400

    Storing archived algorithm challenges from previous FreeCodeCamp curriculum

---
+++ Javascript Algorithms And Data Structures Certification/Archived Advanced Algorithm Scripting/Inventory_Update.js
@@ -0,0 +1,53 @@
+/* Mission:
+ * Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery.
+ * Update the current existing inventory item quantities (in arr1).
+ * If an item cannot be found, add the new item and quantity into the inventory array. 
+ * The returned inventory array should be in alphabetical order by item.
+ *
+ * Resouces:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+
+function compare(x, y) {
+  if (x[1] < y[1])
+    return -1;
+  else if (x[1] > y[1])
+    return 1;
+  else 
+    return 0;
+}
+
+function updateInventory(arr1, arr2) {
+  for(i=0;i<arr2.length;i++){
+    var exist = false;
+    for(j=0;j<arr1.length;j++)
+      if(arr2[i][1] == arr1[j][1]){
+        exist = true;
+        arr1[j][0] += arr2[i][0]; 
+        break;
+      }
+        
+    if(exist == false)
+      arr1.push(arr2[i]);
+  }
+  return arr1.sort(compare);
+}
+
+/* =============================================================================
+ * Example Test Cases
+ * =============================================================================
+ */
+updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]).length;
+//should return an array with a length of 6
+
+updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]);
+//should return [[88, "Bowling Ball"], [2, "Dirty Sock"], [3, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]]
+
+updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], []);
+//should return [[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]]
+
+updateInventory([], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]); 
+//should return [[67, "Bowling Ball"], [2, "Hair Pin"], [3, "Half-Eaten Apple"], [7, "Toothpaste"]]
+
+updateInventory([[0, "Bowling Ball"], [0, "Dirty Sock"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]);
+//should return [[1, "Bowling Ball"], [0, "Dirty Sock"], [1, "Hair Pin"], [1, "Half-Eaten Apple"], [0, "Microphone"], [1, "Toothpaste"]]
---
+++ Javascript Algorithms And Data Structures Certification/Archived Advanced Algorithm Scripting/No_Repeat_Please.js
@@ -0,0 +1,49 @@
+/* Mission:
+ * Return the number of total permutations of the provided string that don't have repeated consecutive letters.
+ * Assume that all characters in the provided string are each unique.
+ *
+ * Resources:
+ * https://www.mathsisfun.com/combinatorics/combinations-permutations.html
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+ */
+ 
+var repeatOnly = /^([a-z])\1+$/;
+var repeatStr = /([a-z])\1+/;
+
+function permNumbers(string){
+  var results = [];
+  if(string.length == 1)
+    results.push(string);
+  else
+    for(var i=0;i<string.length;i++){
+      var remaining = permNumbers(string.substring(0,i) + string.substring(i+1));
+      for(var j=0;j<remaining.length;j++)
+        if(!(repeatStr.test(string[i] + remaining[j])))
+          results.push(string[i] + remaining[j]);
+    }
+  return results;
+}
+
+function permAlone(string) {
+  if(string.length == 1)
+    return 1;
+  else if(repeatOnly.test(string))
+    return 0;
+  else
+    return permNumbers(string).length;
+}
+
+/* =============================================================================
+ * Example Test Cases
+ * =============================================================================
+ */
+permAlone("aab");       //should return a number
+permAlone("aab");       //should return 2
+permAlone("aaa");       //should return 0
+permAlone("aabb");      //should return 8
+permAlone("abcdefa");   //should return 3600
+permAlone("abfdefa");   //should return 2640
+permAlone("zzzzzzzz");  //should return 0
+permAlone("a");         //should return 1
+permAlone("aaab");      //should return 0
+permAlone("aaabb");     //should return 12
